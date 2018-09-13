a2a9ef333d94597ff57c3d3a25fbf8583366b8b2
Travis Tressler <r.travis.tressler@gmail.com>
2018-09-12 20:09:08 -0400

    Butterknife bindings for form finished.

--- app/src/main/java/com/rsp/ndvd/view/importview/ImportFragment.java
+++ app/src/main/java/com/rsp/ndvd/view/importview/ImportFragment.java
@@ -3,20 +3,52 @@ package com.rsp.ndvd.view.importview;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rsp.ndvd.R;
 import com.rsp.ndvd.viewUtils.Constants;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ImportFragment extends Fragment implements ImportView {
 
+    @BindView(R.id.input_first_name)
+    protected TextInputEditText firstNameInput;
+
+    @BindView(R.id.input_last_name)
+    protected TextInputEditText lastNameInput;
+
+    @BindView(R.id.input_email)
+    protected TextInputEditText emailInput;
+
+    @BindView(R.id.input_mobile_phone)
+    protected TextInputEditText mobilePhoneInput;
+
+    @BindView(R.id.input_home_phone)
+    protected TextInputEditText homePhoneInput;
+
     private ImportPresenter presenter;
     private ImportFragment importFragment;
+
+    @OnClick(R.id.button_save_form)
+    protected void onSaveClicked(View view) {
+
+    }
+
+    @OnClick(R.id.button_take_picture)
+    protected void onTakePictureClicked(View view) {
+        
+    }
 
     @Nullable
     @Override
